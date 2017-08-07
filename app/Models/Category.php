db7d013479d2c9173a547d253fc824808b18182b
Maxime Leclerc <contact@maxime-leclerc.com>
2017-08-08 00:32:53 +0200

    Add polymorphic relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -22,4 +22,20 @@ class Category extends Model
     {
         return $this->belongsToMany(Post::class);
     }
+
+    /**
+     * Get all of the url's categories.
+     */
+    public function urls()
+    {
+        return $this->morphMany(Url::class, 'urlable');
+    }
+
+    /**
+     * Get all of the file's categories.
+     */
+    public function files()
+    {
+        return $this->morphMany(File::class, 'filable');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,4 +31,20 @@ class Post extends Model
     {
         return $this->belongsToMany(Tag::class);
     }
+
+    /**
+     * Get all of the url's categories.
+     */
+    public function urls()
+    {
+        return $this->morphMany(Url::class, 'urlable');
+    }
+
+    /**
+     * Get all of the file's categories.
+     */
+    public function files()
+    {
+        return $this->morphMany(File::class, 'filable');
+    }
 }
